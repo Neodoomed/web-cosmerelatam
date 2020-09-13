4c46d3cccfc62a39572bbbebbe6645493b59f08c
Neodoomed <999.doom@gmail.com>
2020-09-12 23:20:12 -0300

    new update

--- src/component/Noticias/Noticias.js
+++ src/component/Noticias/Noticias.js
@@ -5,10 +5,18 @@ class Noticias extends Component{
     render(){
         return(
             <section className="noticias">
-                <div className="jumbotron">
-                    <h1 className="display-3">Secccion de noticias.</h1>
-                    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr className="my-4"></hr>
+                <h1 className="display-3">Secccion de noticias</h1>
+                <div className="news_main">
+                    <div className="news_swiper">
+
+                    </div>
+                    <div className="news_conteiner">
+                        <ul className="news_tab">
+                            <li class="tab_item">Ultimas</li>
+                            
+                        </ul>
+                    </div>
+
                 </div>
             </section>
         )
