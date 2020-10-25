5827230ec06109ebf6956d2a623499a2df9f025b
Neodoomed <999.doom@gmail.com>
2020-10-25 15:30:07 -0300

    update

---
+++ components/Socials.js
@@ -0,0 +1,32 @@
+export const SocialItems = [
+    {
+        social:'Twitter',
+        url: 'https://twitter.com/CosmereLatam',
+        cName: 'tw',
+        cIcon: 'tw_icon'
+    },
+    {
+        social:'Facebook',
+        url: 'https://www.facebook.com/cosmerelatam',
+        cName: 'fb',
+        cIcon: 'fb_icon'
+    },
+    {
+        social:'Instagram',
+        url: 'https://www.instagram.com/cosmerelatam/',
+        cName: 'ig',
+        cIcon: 'ig_icon'
+    },
+    {
+        social:'Youtube',
+        url: 'https://www.youtube.com/channel/UCjnXHtHI0L4rfvTWccLuebQ',
+        cName: 'yt',
+        cIcon: 'yt_icon'
+    },
+    {
+        social:'Discord',
+        url: '#',
+        cName: 'dc',
+        cIcon: 'dc_icon'
+    }
+]
