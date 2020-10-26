924b96f719629208f68dbf87bef7b7c819810a36
Neodoomed <999.doom@gmail.com>
2020-10-26 01:16:04 -0300

    update

--- pages/shadesmar.jsx
+++ pages/shadesmar.jsx
@@ -1,50 +1,16 @@
+import {globalStyles, loginStyles} from '../styles/shadesmarStyles'
 
 export default function Shadesmar(){
     return(
         <>
         <section className="Shadesmar">
             <div className="main">
-                <h1>Has estrado en Shadesmar.</h1>
-                <h2>Desde aqui puedes moldar el mundo.</h2>
+                <h1>Has entrado en Shadesmar.</h1>
+                <h5>Desde aqui puedes moldar el mundo.</h5>
             </div>
         </section>
-        
-        <style jsx>{`
-            *{
-                margin: 0;
-                padding: 0;
-            }
-            section{
-                position: relative;
-                min-width: 100%;
-                min-height: 100vh;
-                background: url("/images/shadesmar.jpg");
-                background-size: cover;
-                background-position: left;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-            .main{
-                position: relative;
-                background-color: rgba(200, 200, 200, .5);
-                min-width: 100%;
-                min-height: 100vh;
-                margin: auto;
-                border-radius: 5px;
-                padding: 40px 20px;
-            }
-
-            @media all and (min-width: 768px){
-            .main{
-                
-                min-width: 600px;
-                height: 90vh;
-                min-height: 90vh;
-            }
-}
-
-        `}</style>
+        <style jsx global>{globalStyles}</style>
+        <style jsx>{loginStyles}</style>
         </>
     );
 }
---
+++ styles/shadesmarStyles.js
@@ -0,0 +1,59 @@
+import css from 'styled-jsx/css'
+
+const color = {
+    primary: '#393b40',
+    secondary: '#939313',
+    
+
+}
+
+export const globalStyles = css.global`
+    html,
+    body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+
+    * {
+    box-sizing: border-box;
+    }
+`
+
+export const loginStyles = css`
+    section{
+        position: relative;
+        min-width: 100%;
+        min-height: 100vh;
+        background: url("/images/shadesmar.jpg");
+        background-size: cover;
+        background-position: left;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .main{
+        position: relative;
+        background-color: rgba(200, 200, 200, .5);
+        min-width: 100%;
+        min-height: 100vh;
+        margin: auto;
+        border-radius: 5px;
+        padding: 40px 20px;
+    }
+
+    @media all and (min-width: 768px){
+    .main{
+        
+        min-width: 600px;
+        height: 90vh;
+        min-height: 90vh;
+    }
+}
+`
